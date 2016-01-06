f407ea18963e6f4bfe826cf2ecbc7695e716d94e
Donald Leiva <admin@donaldleiva.com>
2016-01-05 21:01:47 -0600

    + Archivos faltantes

---
+++ website/application/modules/client/models/content_model.php
@@ -0,0 +1,42 @@
+<?php
+/**
+  * Content model
+  *
+  * This is the main model for content on the administration part.
+  *
+  * @author  	Donald Leiva <admin@donaldleiva.com>
+  * @package	Admin/Content/Models
+  */
+class Content_model extends CI_Model {
+
+	/**
+	 * Constructor for this controller.
+	 *
+	 */
+
+    function  __construct() {
+        parent::__construct();
+    }
+
+	/**
+	 * Get a specific law
+	 *
+	 * @param	int			Content ID
+	 * @return	array		Content information
+	 */		
+	function get($content_id)
+	{
+		$this->db->select('c.*')
+			->from('content c')
+			->where('c.content_id', $content_id);
+	
+        $query = $this->db->get();
+
+        if ($query->num_rows() > 0)
+		{
+			return $query->row();
+		}
+        else
+            return FALSE;
+	}
+}
